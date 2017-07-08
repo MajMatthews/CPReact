ce2963d2423f9a4c962f8bedd6e8568660a8b490
Craig Matthews <mrcraigmatthews@hotmail.co.uk>
2017-07-08 15:07:49 +0100

    Style alteration and change to display setup - working mainly on content.

--- client/components/Details.js
+++ client/components/Details.js
@@ -5,12 +5,21 @@ export default class Details extends React.Component {
 	render() {
 		const {doc, i} = this.props;
 		const pathToImage = doc.Graphic;
-		
+		let snippet, image;
+		if (doc.Graphic !== undefined || null) {
+			image =  <img src={require('../images/' + pathToImage)} />
+		};
+
+		if (doc.snippet !== undefined || null) {
+			snippet =  <div className="snippet" dangerouslySetInnerHTML={ {__html: doc.snippet} } />
+		};
+		console.log('snippet', snippet, 'image', image);
 		return (
 		    <div className="details">
 		    	<h2>{doc.Title}</h2>
-		    	<p>{doc.Content}</p>
-				<img src={require('../images/' + pathToImage)} />
+		    	<div dangerouslySetInnerHTML={ {__html: doc.Content} } />
+				{snippet}
+				{image}
 	      	</div>	
 		)	
 	}
--- client/data/docs.js
+++ client/data/docs.js
@@ -3,8 +3,9 @@ const docs = [
       "Title":"Es6",
       "selector":"es6",
       "tab":"Es6",
-      "Content":"Es6 functionality, at ContactPartners Es6 is used at the centre of the development process. Babel is used to enable the Javascript to work on browsers which are not able to work properly with Es6.",
+      "Content":"<div className=\"Content\"><div><p>Es6 functionality, at ContactPartners Es6 is used at the centre of the development process. </p><p>Babel is used to enable the Javascript to work on browsers which are not able to work properly with Es6.</p><p> Advantages of Es6 are great with the introductions of, let, const and blocked scoping which helps in leading to less misunderstandings that was caused by Es5.</p><p>Component classes are written using the es6 syntax, much like use of arrow functions and the declaring of variables with const and let throughout development.</p><p>Please see and example of Es6 class decloration below.</p></div>",
       "Graphic":"blank.png",
+      "snippet":"<div className=\"style\"><p>export default class Header extends Component {<br/>  &nbsp&nbsp&nbsp&nbsp constructor(props) {<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp super(props);<br/> &nbsp&nbsp&nbsp&nbsp  }<br/>&nbsp&nbsp&nbsp&nbsp&nbsp    render() {<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp        const {children} = this.props;<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp       return ( <br/> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp            &ltAppBar  className={styles.appBar} ><br/> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp               {children}<br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp            &lt/AppBar><br/>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp        );<br/>&nbsp&nbsp&nbsp }<br/>}</p></div>",
       "id":"1160844458347054781"
    },
    {
@@ -96,11 +97,12 @@ const docs = [
       "id":"1141561999742846119"
    },
    {
-      "Title":"Tech Stack Highlight",
+      "Title":"Tech Stack Highlights",
       "selector":"techstackhighlights",
       "tab":"Tech Stack Highlight",
       "Content":"It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      "Graphic":"th.png",
+      "snippet":"<div>immutable: '^3.8.1',<br/>    'moment': '^2.18.1',<br/>     'react': '^15.4.2','<br/>     'react-dom': '^15.4.2','<br/>    'react-router': '^4.1.1','<br/>     'react-router-dom': '^4.0.0','<br/>    'react-toolbox': '^2.0.0-beta.7','<br/>      'reflux': '^6.4.1','<br/>     'json-server': '^0.9.6', <br/>      'postcss': '^5.2.16','<br/>      'webpack': '^2in process of upgrading to 3', '<br/>      'Eslint' : majestic -  based on airbnb lint guidelines, '<br/>     'Testing': Minimal current utilisation<div>",
+      "Graphic":"blank.png",
       "id":"1137894817632733056"
    },
    {
